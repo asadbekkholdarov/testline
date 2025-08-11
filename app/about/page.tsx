47fa886078d7ee80cd2f7b1c40eb99ddcab29a42
asadbek kholdarov <78337069+asadbekkholdarov@users.noreply.github.com>
2025-08-12 00:50:57 +0500

    Updated page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -145,19 +145,23 @@ export default function About() {
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-       <Card className="group hover:shadow-lg transition-shadow duration-300 p-0 rounded-none border-none">
-  <div className="w-full relative">
-    <Image
-      src="/certifications/images/HBs_Ag_Cassette.jpg"
-      alt="HBs Ag Cassette Certificate"
-      width={0}
-      height={0}
-      sizes="100vw"
-      className="w-full h-auto object-contain rotate-[270deg] group-hover:scale-105 transition-transform duration-300"
-    />
-  </div>
-</Card>
-
+            <Card className="group hover:shadow-lg transition-shadow duration-300">
+              <CardHeader className="text-center">
+                <div className="w-full h-full relative mb-4 overflow-hidden rounded-lg">
+                  <Image
+                    src="/certifications/images/HBs_Ag_Cassette.jpg"
+                    alt="HBs Ag Cassette Certificate"
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    className="w-full h-auto group-hover:scale-105 transition-transform duration-300 rotate-[270deg]"
+                  />
+                </div>
+                <CardTitle className="text-lg text-gray-900">
+                  HBs Ag Cassette
+                </CardTitle>
+              </CardHeader>
+            </Card>
 
             <Card className="group hover:shadow-lg transition-shadow duration-300">
               <CardHeader className="text-center">
